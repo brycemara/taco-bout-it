17b790ad347ab32370667930e9c895c28c6953fc
Bryce Mara Jarrett <brycej98@gmail.com>
2021-01-14 17:08:57 -0600

    Add reviews testing, ticket #11

--- src/ReviewForm/ReviewForm.test.js
+++ src/ReviewForm/ReviewForm.test.js
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import ReviewForm from "./ReviewForm";
 
