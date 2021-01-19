f16ec2a6ce15683cf45e1bf992383965040953fe
Bryce Mara Jarrett <brycej98@gmail.com>
2021-01-18 18:52:41 -0600

    Add an "s" to http in fetch

--- src/APIcalls.js
+++ src/APIcalls.js
@@ -1,4 +1,4 @@
 export const fetchTaco = async () => {
-  const response = await fetch('http://taco-randomizer.herokuapp.com/random/')
+  const response = await fetch('https://taco-randomizer.herokuapp.com/random/')
   return await response.json()
 }
